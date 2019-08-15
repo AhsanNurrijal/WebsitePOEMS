d0e3bd14c9950811665bdd522d49de54d33f2036
Ahsan Nurrijal <ahsannurrijal77@gmail.com>
2019-08-15 15:21:47 +0700

    commit

---
+++ Poems/js/script.js
@@ -0,0 +1,18 @@
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	}
+
+function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
+}
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
